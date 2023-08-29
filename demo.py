73175d5b06bf529c139fa1b75db14feea507d33f
TheBigBlockPC <jeremy.naumann2007@gmail.com>
2023-08-29 11:35:37 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,43 +1,57 @@
 import openAI_chatbots as chat
+
+# Set your OpenAI API key
 chat.setApiKey("<your api key>")
+
+# Create a chatbot instance
 chatbot = chat.chatbot()
+
+# Create a function calling object
 functions = chat.GPT_Functions.GPT_functions()
-@functions.defineFunction("reads a file",{
-    "type":"string",
-    "name":"Path",
-    "description":"the file path"
+
+# Define a function to read a file
+@functions.defineFunction("reads a file", {
+    "type": "string",
+    "name": "Path",
+    "description": "the file path"
 })
 def ReadFile(Path):
     content = ""
     try:
-        with open(Path,"r") as f:
+        with open(Path, "r") as f:
             content = f.read()
-        return "content:"+content
+        return "content:" + content
     except:
-        return "error"
+        return "error reading file"
 
-@functions.defineFunction("writes to a file",{
-    "type":"string",
-    "name":"Path",
-    "description":"the file path"
-},{
-    "type":"string",
-    "name":"Content",
-    "description":"the content of the file"
+# Define a function to write to a file
+@functions.defineFunction("writes to a file", {
+    "type": "string",
+    "name": "Path",
+    "description": "the file path"
+}, {
+    "type": "string",
+    "name": "Content",
+    "description": "the content of the file"
 })
-def WriteFile(Path,content):
+def WriteFile(Path, content):
     try:
-        with open(Path,"w") as f:
+        with open(Path, "w") as f:
             f.write(content)
-        return "file written to"
+        return "file written successfully"
     except:
-        return "error"
+        return "error writing file"
+
+# Bind the function calling object to the chatbot
 chatbot.setGptFunctions(functions)
+
+# Chat loop
 Running = True
 while Running:
-    prompt = input("user: ")
+    prompt = input("You: ")
     if prompt == "end":
         Running = False
-        print("chat has ended")
+        print("Chat has ended")
     else:
-        print("AI: ",chatbot.chat(prompt))
+        response = chatbot.chat(prompt)
+        print("AI:", response)
